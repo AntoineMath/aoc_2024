7c9e098ba537575c46a37193c3152542b5f428a7
Antoine Mathurin <antoine.mathurin.external@fr.airbus.com>
2024-12-06 16:25:47 +0100

    [WIP] day6 part2

--- day6.py
+++ day6.py
@@ -11,7 +11,6 @@ def part1(grid) -> int:
     "v": "<",
     "<": "^"
   }
-  print(grid)
   ROWS, COLS = len(grid), len(grid[0])
   visited = set()
   guard_dir = ""
@@ -39,14 +38,70 @@ def part1(grid) -> int:
   return len(visited)
 
 
+def walk(grid):
+  dirs = {
+    "^": (-1, 0),
+    ">": (0, 1),
+    "v": (1, 0),
+    "<": (0, -1)
+  }
+  rotations = {
+    "^": ">",
+    ">": "v",
+    "v": "<",
+    "<": "^"
+  }
+  ROWS, COLS = len(grid), len(grid[0])
+  visited = set()
+  path = []
+  guard_dir = ""
+  start_pos = (-1, -1)
+  for r in range(ROWS):
+    for c in range(COLS):
+      if grid[r][c] in "<^>v":
+        start_pos = r, c
+        guard_dir = grid[r][c]
+
+
+  # 1- compute path (visited)
+  x, y = start_pos
+  while 0 <= x < ROWS and 0 <= y < COLS:
+
+    if (x, y, rotations[guard_dir]) in visited:
+      obstacles += 1
+      dx, dy = dirs[guard_dir]
+      print(x+dx, y+dy)
+    if grid[x][y] == "#":
+      dx, dy = dirs[guard_dir]
+      x, y = x - dx, y - dy # one step back
+      guard_dir = rotations[guard_dir]
+    else:
+      visited.add((x, y, guard_dir))
+      path.append((x, y, guard_dir))
+    dx, dy = dirs[guard_dir]
+    x, y = x + dx, y + dy
+  
+
+def part2(grid) -> int:
+  """
+   intuition: for each position along the way, if rotating 90deg (aka adding an obstacle)
+   would put you in a previous position with the same previous direction 
+   Then it means you'll be in a loop
+   """
+  obstacles = 0
   
 
 
+  # 2 - replay path, but replace 1 position by an obstacle
+  # and check if one position is reached again
+  for i in range(path):
+    cur_grid = grid[::]
 
 
-def part2(lines) -> int:
-  pass
 
+    
+
+  return obstacles
 
 if __name__ == "__main__":
   with open("inputs/day6", "r") as f:
@@ -54,5 +109,5 @@ if __name__ == "__main__":
 
   grid = [l.strip() for l in lines]
   
-  print(part1(grid))
-  print(part2(lines))
+  #print(part1(grid))
+  print(part2(grid))
