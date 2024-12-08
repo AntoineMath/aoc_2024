7d0147603134932f07bd1192f40e4cf061871513
AntoineMath <antoine.mathu@gmail.com>
2024-12-08 22:14:03 +0100

    day6 bruteforce ugly

--- day6.py
+++ day6.py
@@ -1,16 +1,18 @@
+
+dirs = {
+  "^": (-1, 0),
+  ">": (0, 1),
+  "v": (1, 0),
+  "<": (0, -1)
+}
+rotations = {
+  "^": ">",
+  ">": "v",
+  "v": "<",
+  "<": "^"
+}
+
 def part1(grid) -> int:
-  dirs = {
-    "^": (-1, 0),
-    ">": (0, 1),
-    "v": (1, 0),
-    "<": (0, -1)
-  }
-  rotations = {
-    "^": ">",
-    ">": "v",
-    "v": "<",
-    "<": "^"
-  }
   ROWS, COLS = len(grid), len(grid[0])
   visited = set()
   guard_dir = ""
@@ -38,70 +40,56 @@ def part1(grid) -> int:
   return len(visited)
 
 
-def walk(grid):
-  dirs = {
-    "^": (-1, 0),
-    ">": (0, 1),
-    "v": (1, 0),
-    "<": (0, -1)
-  }
-  rotations = {
-    "^": ">",
-    ">": "v",
-    "v": "<",
-    "<": "^"
-  }
-  ROWS, COLS = len(grid), len(grid[0])
-  visited = set()
-  path = []
-  guard_dir = ""
-  start_pos = (-1, -1)
-  for r in range(ROWS):
-    for c in range(COLS):
-      if grid[r][c] in "<^>v":
-        start_pos = r, c
-        guard_dir = grid[r][c]
+def is_cycle(start_x, start_y, grid):
+    ROWS, COLS = len(grid), len(grid[0])
+    visited = set()
 
+    x, y = start_x, start_y
+    guard_dir = grid[start_x][start_y]
 
-  # 1- compute path (visited)
-  x, y = start_pos
-  while 0 <= x < ROWS and 0 <= y < COLS:
+    while 0 <= x < ROWS and 0 <= y < COLS:
+        if (x, y, guard_dir) in visited:
+            return True # infinite loop
+        visited.add((x, y, guard_dir))
 
-    if (x, y, rotations[guard_dir]) in visited:
-      obstacles += 1
-      dx, dy = dirs[guard_dir]
-      print(x+dx, y+dy)
-    if grid[x][y] == "#":
-      dx, dy = dirs[guard_dir]
-      x, y = x - dx, y - dy # one step back
-      guard_dir = rotations[guard_dir]
-    else:
-      visited.add((x, y, guard_dir))
-      path.append((x, y, guard_dir))
-    dx, dy = dirs[guard_dir]
-    x, y = x + dx, y + dy
-  
+        if grid[x][y] == "#":  
+            dx, dy = dirs[guard_dir]
+            x, y = x - dx, y - dy  
+            guard_dir = rotations[guard_dir] 
+
+        dx, dy = dirs[guard_dir]
+        x, y = x + dx, y + dy
+    return False
 
 def part2(grid) -> int:
-  """
-   intuition: for each position along the way, if rotating 90deg (aka adding an obstacle)
-   would put you in a previous position with the same previous direction 
-   Then it means you'll be in a loop
-   """
-  obstacles = 0
-  
+  start_x, start_y = (-1, -1)
+  for r in range(len(grid)):
+      for c in range(len(grid[0])):
+          if grid[r][c] in "<^>v":
+              start_x, start_y = r, c
+              break
+
+  obstacle_positions = set()
+
+  guard_start = (start_x, start_y)
+
+  # brute force ugly
+  for x in range(len(grid)):
+      for y in range(len(grid[0])):
+          if grid[x][y] == "#" or (x, y) == guard_start:
+              continue
+
+          curr_grid = grid[:]
+          modified_line = curr_grid[x][:y] + "#" + curr_grid[x][y + 1:]
+          curr_grid = curr_grid[:x] + [modified_line] + curr_grid[x + 1:]
+
+          if is_cycle(start_x, start_y, curr_grid):  # detect cycle
+              obstacle_positions.add((x, y))
+
+  return len(obstacle_positions)
 
 
-  # 2 - replay path, but replace 1 position by an obstacle
-  # and check if one position is reached again
-  for i in range(path):
-    cur_grid = grid[::]
 
-
-
-    
-
-  return obstacles
 
 if __name__ == "__main__":
   with open("inputs/day6", "r") as f:
@@ -109,5 +97,5 @@ if __name__ == "__main__":
 
   grid = [l.strip() for l in lines]
   
-  #print(part1(grid))
+  print(part1(grid))
   print(part2(grid))
